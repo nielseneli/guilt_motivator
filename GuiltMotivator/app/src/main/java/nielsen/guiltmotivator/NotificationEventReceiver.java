63fe1fbaaae469a6b92d71190266f3d2c50285e9
DHZBill <haozheng.du@students.olin.edu>
2016-12-14 22:51:45 -0500

    Added comments

--- GuiltMotivator/app/src/main/java/nielsen/guiltmotivator/NotificationEventReceiver.java
+++ GuiltMotivator/app/src/main/java/nielsen/guiltmotivator/NotificationEventReceiver.java
@@ -13,12 +13,14 @@ import java.util.Date;
 
 /**
  * Created by DHZ_Bill on 11/29/16.
+ * This class handles receiving notifications.
  */
 public class NotificationEventReceiver extends WakefulBroadcastReceiver{
 
     private static final String ACTION_START_NOTIFICATION_SERVICE = "ACTION_START_NOTIFICATION_SERVICE";
     private static final String ACTION_DELETE_NOTIFICATION = "ACTION_DELETE_NOTIFICATION";
 
+    // set up alarm based on the data from SQLite.
     @SuppressLint("NewApi")
     public static void setupAlarm(Context context) {
         //get helper and get db in write mode
@@ -27,6 +29,7 @@ public class NotificationEventReceiver extends WakefulBroadcastReceiver{
         ArrayList<Task> tasks = mDbHelper.getAllTasks();
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         ArrayList<PendingIntent> intentArray = new ArrayList<PendingIntent>();
+        // for each task, set up an alarm according to its due date.
         for(int i = 0; i < tasks.size(); i++) {
             PendingIntent alarmIntent = getStartPendingIntent(context,i);
             alarmManager.setExact(AlarmManager.RTC_WAKEUP,
@@ -36,34 +39,40 @@ public class NotificationEventReceiver extends WakefulBroadcastReceiver{
         }
     }
 
+    // handle receiving notifications
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         Intent serviceIntent = null;
+        // when the task is due, create the notification service
         if (ACTION_START_NOTIFICATION_SERVICE.equals(action)) {
             Log.i(getClass().getSimpleName(), "onReceive from alarm, starting notification service");
             serviceIntent = NotificationIntentService.createIntentStartNotificationService(context);
-        } else if (ACTION_DELETE_NOTIFICATION.equals(action)) {
+        }
+        // when deleting the task, create the intent for deleting notification
+        else if (ACTION_DELETE_NOTIFICATION.equals(action)) {
             Log.i(getClass().getSimpleName(), "onReceive delete notification action, starting notification service to handle delete");
             serviceIntent = NotificationIntentService.createIntentDeleteNotification(context);
         }
+        // if no service, start it.
         if (serviceIntent != null) {
             startWakefulService(context, serviceIntent);
         }
     }
-
+    // return the date to notify.
     private static long getTriggerAt(Date dueDate) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(dueDate);
         return calendar.getTimeInMillis();
     }
 
+    // get the intent for starting notification
     private static PendingIntent getStartPendingIntent(Context context, int i) {
         Intent intent = new Intent(context, NotificationEventReceiver.class);
         intent.setAction(ACTION_START_NOTIFICATION_SERVICE);
         return PendingIntent.getBroadcast(context, i, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
-
+    // get the intent for deleting notificatio.
     public static PendingIntent getDeleteIntent(Context context) {
         Intent intent = new Intent(context, NotificationEventReceiver.class);
         intent.setAction(ACTION_DELETE_NOTIFICATION);
